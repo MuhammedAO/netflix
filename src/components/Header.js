28d4c4a7350f9c8d586f65ff257d18d2a81804be
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-05 17:41:38 +0100

    Header component completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import logo from '../assets/svg/logo.svg'
-import  {NavLink } from 'react-router-dom'
+import  {NavLink , Link} from 'react-router-dom'
 import styled from 'styled-components'
+import {Icon} from 'react-icons-kit'
+import {ic_keyboard_arrow_right} from 'react-icons-kit/md/ic_keyboard_arrow_right'
 
 class Header extends Component {
  render() {
@@ -10,6 +12,15 @@ class Header extends Component {
      <div className="header-top">
      <Logo src={logo} alt="logo"/>
      <NavLink className="signin-btn" to="">Sign in</NavLink>
+     </div>
+    {/* Header content*/}
+     <div className="header-content">
+     <Title>See what's next.</Title>
+     <SubTitle>WATCH ANYWHERE. CANCEL ANYTIME.</SubTitle>
+     <Link className="main-btn" to="">
+     try it now
+     <Icon className="icon" icon={ic_keyboard_arrow_right} size={37}/>
+     </Link>
      </div>
     </HeaderComponent>
   )
@@ -38,12 +49,52 @@ transition: background 0.2s ease-in;
 }
 }
 
+.icon{
+ vertical-align: bottom;
+ margin-left: 1.5rem;
+}
+
 /* Header Top */
 .header-top{
- 
+
 position: relative;
 height:10rem;
 z-index:1;
+
+}
+
+/* Header content */
+.header-content{
+ width: 65%;
+ position:relative;
+ margin: 4.5rem auto 0;
+ display: flex;
+ justify-content: center;
+ align-content: center;
+ text-align: center;
+ flex-direction: column;
+ z-index:1;
+}
+
+/* Main Btn */
+.main-btn{
+ display: inline-block;
+ background: var(--main-red);
+ text-transform: uppercase;
+ border:none;
+ outline:none;
+ margin: 0 33%;
+ padding: 1.5rem;
+border-radius: 0.1875rem;
+font-size:2rem;
+text-align: center;
+box-shadow: 0 1px 0 rgba(0,0,0,0.45);
+transition: background 0.2s ease-in;
+cursor: pointer;
+
+&:hover{
+background: var(--main-red-hover)
+}
 
 }
 
@@ -57,5 +108,23 @@ const Logo = styled.img`
  top:25%;
  left:50%;
  transform: translate(-50%, -50%);
+
+`
+
+// Main title
+const Title = styled.h1`
+margin: 0 0 1.2rem;
+font-size: 5rem;
+font-weight: 700;
+line-height: 1.1em;
+`
+
+//SubTitle
+const SubTitle = styled.h2`
+font-weight:400;
+font-size:1.875rem;
+line-height:1.25rem;
+margin: 0 0 1.875rem;
+text-transform:uppercase;
 
 `
