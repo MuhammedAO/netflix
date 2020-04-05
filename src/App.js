d6e20757d6ca415261d67bda024d4cf4d0a1b1c0
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-05 15:27:16 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import {Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <Switch>
-    <div >
      <Route exact path="/" component={Main}/>
-    </div>
     </Switch>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import Header from '../components/Header'
 
- class Main extends Component {
-    render() {
-        return (
-            <div>
-            <h1>Parent component</h1>  
-            </div>
-        )
-    }
+class Main extends Component {
+ render() {
+  return (
+   <div>
+     <Header/>
+   </div>
+  )
+ }
 }
 
 
