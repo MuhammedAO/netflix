e6d4749cfb60edb7077e5046083a6ac979ee7f1d
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-05 20:25:11 +0100

    conditional style rendering

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -6,22 +6,25 @@ import ThirdTab from './tabs_nav/ThirdTab'
 import '../css/TabsNav.css'
 
 class TabComponent extends Component {
+    state={
+        tabIndex: 0
+    }
  render() {
   return (
    <div>
-    <Tabs className="tabs">
+    <Tabs className="tabs" selectedIndex={this.state.tabIndex} onSelect={tabIndex => this.setState({tabIndex})}>
      <TabList className="tab-nav">
-      <Tab  >
+      <Tab className={`${this.state.tabIndex === 0 ? 'active' : null} `}>
        <FirstTab />
        <p> <strong>No commitments <br /> cancel online at anytime. </strong> </p>
       </Tab>
-      <Tab>
+      <Tab className={`${this.state.tabIndex === 1 ? 'active' : null} `}>
        <SecondTab />
        <p style={{ marginTop: '-5.3125rem' }}>
         <strong>Watch anywhere</strong>
        </p>
       </Tab>
-      <Tab >
+      <Tab className={`${this.state.tabIndex === 2 ? 'active' : null} `}>
        <ThirdTab />
        <p> <strong>pick your price</strong> </p>
       </Tab>
