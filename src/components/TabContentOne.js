6f50a3c354411cf09c309908966adea4608064dd
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-09 18:04:21 +0100

    responsiveness with s-m-q

--- src/components/TabContentOne.js
+++ src/components/TabContentOne.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Img from '../assets/images/tab-1-pic.png'
 import styled from 'styled-components'
 import { Button } from './Button'
+import {generateMedia} from 'styled-media-query'
 
 export default function TabContentOne() {
  return (
@@ -9,7 +10,7 @@ export default function TabContentOne() {
    <div className="container">
     <div className="tab-content">
      <div>
-      <span style={{ marginBottom: '2rem' }}>if you decide netflix isn't for you - no problem. No commitment. Cancel online anytime.</span>
+      <span className="title" style={{ marginBottom: '2rem' }}>if you decide netflix isn't for you - no problem. No commitment. Cancel online anytime.</span>
       <br />
       <Button style={{ marginTop: '2rem' }}>try it now</Button>
      </div>
@@ -19,6 +20,12 @@ export default function TabContentOne() {
   </TabContentContainer>
  )
 }
+
+//Media Query
+const customMedia = generateMedia({
+    smDesktop:'1440px',
+    tablet: '960px'
+})
 
 const TabContentContainer = styled.div`
 background: var(--main-deep-dark);
@@ -38,6 +45,21 @@ img {
  align-items: center;
  font-size: 2rem;
  padding: 2.5rem;
+ ${customMedia.lessThan('tablet')`
+    display: grid;
+    grid-template-columns: 100%;
+    text-align: center;
+    padding-left: 0;
+    padding-right: 0;
+    `}
+}
+
+.title{
+    margin-top: 2rem;
+    ${customMedia.lessThan('smDesktop')`
+    font-size: 1.5rem;
+    line-height: 1;
+    `}
 }
 
 `
