0b3a070ed3be8e9bb8578e70e388f0678bcc6d8b
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-04-13 13:53:14 +0100

    login page

---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import logo from '../assets/svg/logo.svg'
+import styled from 'styled-components'
+import LoginForm from '../components/login/LoginForm'
+
+const Login = () => {
+ return (
+  <div className="main-login-container">
+  <div className="header-top">
+  <Logo src={logo} alt="logo" className="logo"/>
+  </div>
+  <LoginForm/>
+  </div>
+ )
+}
+
+export default Login
+
+const Logo= styled.img`
+ width: 11rem;
+ position: absolute;
+ top: 25%;
+ left: 11%;
+ transform: translate(-50%, -50%);
+ margin-left: 0;
+
+`
